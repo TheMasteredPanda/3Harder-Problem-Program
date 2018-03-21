89aea06c6931cb627faec537985fc127150b8554
Duke Jake Morgan <dukejakemorgan@gmail.com>
2018-03-21 06:18:40 +0000

    Added Problem and ProblemBuilder class. Added DataManager class.

--- 3Harder Problem Program/Login Form.cs
+++ 3Harder Problem Program/Login Form.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace _3Harder_Problem_Program
 {
@@ -21,5 +22,169 @@ namespace _3Harder_Problem_Program
         {
 
         }
+    }
+
+    public class DataManager
+    {
+        private SQLiteConnection connection;
+
+        public DataManager()
+        {
+            connection = new SQLiteConnection();
+        }
+
+        public bool save(ProblemBuilder.Problem problem)
+        {
+
+
+            return true;
+        }
+    }
+
+    public class ProblemBuilder
+    {
+        private String problemId;
+        private DateTime time;
+        private String name;
+        private Priority priority;
+        private Type problemType;
+        private String roomId;
+        private String[] description;
+
+        public ProblemBuilder(String problemId)
+        {
+            if (problemId == null)
+            {
+                throw new NoNullAllowedException("problemId is null in ProblemBuilder contructor.");
+            }
+
+            this.problemId = problemId;
+        }
+
+        public ProblemBuilder WithTime(DateTime time)
+        {
+            this.time = time;
+            return this;
+        }
+
+        public ProblemBuilder WithName(String name)
+        {
+            if (name == null)
+            {
+                throw new NoNullAllowedException("Name cannot be null.");
+            }
+
+            this.name = name;
+            return this;
+        }
+
+        public ProblemBuilder WithPriority(Priority priority)
+        {
+            this.priority = priority;
+            return this;
+        }
+
+        public ProblemBuilder WithProblemType(Type type)
+        {
+            problemType = type;
+            return this;
+        }
+
+        public ProblemBuilder WithRoomId(String roomId)
+        {
+            if (roomId == null)
+            {
+                throw new NoNullAllowedException("Room id cannot be null.");
+            }
+
+            this.roomId = roomId;
+            return this;
+        }
+
+        public ProblemBuilder WithDescription(String[] description)
+        {
+            this.description = description;
+            return this;
+        }
+
+        public Problem build()
+        {
+            return new Problem(problemId, time, name, priority, problemType, roomId, description);
+        }
+
+    public class Problem
+    {
+        private String problemId;
+        private DateTime date;
+        private String name;
+        private Priority priority;
+        private Type problemType;
+        private String roomId;
+        private String[] description;
+
+        public Problem(String problemId, DateTime date, String name, Priority priority, Type problemType, String roomId, String[] description)
+        {
+            this.problemId = problemId;
+
+            if (date == null)
+            {
+                date = new DateTime();
+            }
+
+            this.name = name;
+            this.priority = priority;
+            this.problemType = problemType;
+            this.roomId = roomId;
+            this.description = description;
+        }
+
+        public String GetProblemId()
+        {
+            return problemId;
+        }
+
+        public DateTime GetDateTime()
+        {
+            return date;
+        }
+
+        public String GetName()
+        {
+            return name;
+        }
+
+        public Priority GetPriority()
+        {
+            return priority;
+        }
+        
+        public Type GetType()
+        {
+            return problemType;
+        }
+
+        public String GetRoomId()
+        {
+            return roomId;
+        }
+
+        public String[] GetDescription()
+        {
+            return description;
+        }
+    }
+
+    public enum Priority
+    {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public enum Type
+    {
+        HARDWARE,
+        SOFTWARE,
+        TELECOMMUNICATION
     }
 }
